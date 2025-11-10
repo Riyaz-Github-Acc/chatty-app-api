2346c110bc2706389b72e349e465b0b4f2d484f7
Riyaz <riyazofficeid@gmail.com>
2025-11-10 19:25:02 +0530

    fixed the env error

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import express from 'express';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
-import envConfig from './config/env.config.js';
-import { app, server } from './config/socket.config.js';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+import envConfig from "./config/env.config.js";
+import { app, server } from "./config/socket.config.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://chatty-bot-app.netlify.app"],
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 server.listen(envConfig.PORT, () => {
-    console.log(`💻 Server started to run on the port: ${envConfig.PORT}`);
+  console.log(`💻 Server started to run on the port: ${envConfig.PORT}`);
 });
